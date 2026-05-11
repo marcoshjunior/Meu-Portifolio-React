69af9cd4e269f471f9be03da37fe6f9175b8f8fb
marcoshjunior <155772166+marcoshjunior@users.noreply.github.com>
2026-05-11 19:49:20 -0300

    seção de contato

--- portifolio-react/src/components/Contact/Contact.jsx
+++ portifolio-react/src/components/Contact/Contact.jsx
@@ -1,5 +1,29 @@
 import styles from "./Contact.module.css";
+import { profile } from "../../data/portifolio";
 
 export default function Contact() {
-  return;
+  return (
+    <section id="contato" className="section">
+      <div className={`container ${styles.container}`}>
+        <h2 className="sectionTitle">Contato</h2>
+        <p className="sectionSubtitle">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint
+          voluptatem ab possimus qui.
+        </p>
+
+        <a href={`malito: ${profile.email}`} className={styles.emailButton}>
+          Enviar email
+        </a>
+
+        <div className={styles.links}>
+          <a href={profile.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <a href={profile.linkedin} target="_blank" rel="noreferrer">
+            Linkedin
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
