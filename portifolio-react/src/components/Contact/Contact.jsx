cd38d7b7cc0c20616d40d095188e4d0b3addd361
marcoshjunior <155772166+marcoshjunior@users.noreply.github.com>
2026-06-19 13:52:24 -0300

    mudando a seção contato

--- portifolio-react/src/components/Contact/Contact.jsx
+++ portifolio-react/src/components/Contact/Contact.jsx
@@ -1,26 +1,33 @@
 import styles from "./Contact.module.css";
 import { profile } from "../../data/portifolio";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <section id="contato" className="section">
       <div className={`container ${styles.container}`}>
-        <h2 className="sectionTitle">Contato</h2>
-        <p className="sectionSubtitle">
-          Tem uma oportunidade, projeto ou quer trocar uma ideia sobre
-          tecnologia? Entre em contato comigo!
-        </p>
+        <div className={styles.contato}>
+          <h2 className="sectionTitle">Contato</h2>
+          <p className="sectionSubtitle">
+            Estou aberto a novas oportunidades, parcerias ou simplesmente trocar
+            uma ideia sobre tecnologia. Me envie um e-mail e responderei o mais
+            rápido possível.
+          </p>
 
-        <a href={`malito: ${profile.email}`} className={styles.emailButton}>
-          Enviar email
-        </a>
+          <a href={`malito: ${profile.email}`} className={styles.emailButton}>
+            Enviar email
+          </a>
+        </div>
 
         <div className={styles.links}>
           <a href={profile.github} target="_blank" rel="noreferrer">
-            GitHub
+            <FaGithub />
+            <span>GitHub</span>
           </a>
+
           <a href={profile.linkedin} target="_blank" rel="noreferrer">
-            Linkedin
+            <FaLinkedin />
+            <span>Linkedin</span>
           </a>
         </div>
       </div>
